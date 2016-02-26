5d453eebe2a115afcd9a0379473eaa4cf8a015c3
zcf <107290166@qq.com>
2016-02-26 14:20:19 +0800

    worker1 创建 person类

---
+++ Demo/Demo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  Demo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 张超飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ Demo/Demo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Demo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 张超飞. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
