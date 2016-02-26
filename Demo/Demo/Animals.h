c868a73e84f843d9e543aead8ed99e751aba8484
zcf <107290166@qq.com>
2016-02-26 14:25:14 +0800

    work2 创建animals类

---
+++ Demo/Demo/Animals.h
@@ -0,0 +1,13 @@
+//
+//  Animals.h
+//  Demo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 张超飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animals : NSObject
+
+@end
---
+++ Demo/Demo/Animals.m
@@ -0,0 +1,13 @@
+//
+//  Animals.m
+//  Demo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 张超飞. All rights reserved.
+//
+
+#import "Animals.h"
+
+@implementation Animals
+
+@end
